f227457f94b8216507a0feb1ffe041075f736f20
Danielx64 <289220+Danielx64@users.noreply.github.com>
2023-04-09 10:10:33 +1000

    moving stuff around

--- PCFReactMenu/HelloWorld.tsx
+++ PCFReactMenu/HelloWorld.tsx
@@ -1,84 +1,87 @@
 import * as React from 'react';
-import { Label } from '@fluentui/react';
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import { Nav,INavStyles, INavLinkGroup } from '@fluentui/react/lib/Nav';
-const navStyles: Partial<INavStyles> = {
-  root: {
-    width: 208,
-    height: 350,
-    boxSizing: 'border-box',
-    border: '1px solid #eee',
-    overflowY: 'auto',
-  },
-};
-const navLinkGroups: INavLinkGroup[] = [
-  
-  {
-    links: [
-      {
-        name: 'Child link 1',
-        url: 'http://example.com',
-        target: '_blank',
-      },
-      {
-        name: 'Parent link 1',
-        url: '',
-        target: '_blank',
-        expandAriaLabel: 'Show more Parent link 1',
-        links: [
-          {
-            name: 'Child link 1',
-            url: 'http://example.com',
-            target: '_blank',
-          },
-          {
-            name: 'Child link 2',
-            url: 'http://example.com',
-            target: '_blank',
-            expandAriaLabel: 'Show more Child link 2',
-            links: [
-              {
-                name: '3rd level link 1',
-                url: 'http://example.com',
-                target: '_blank',
-              },
-              {
-                name: '3rd level link 2',
-                url: 'http://example.com',
-                target: '_blank',
-              },
-            ],
-          },
-          {
-            name: 'Child link 3',
-            url: 'http://example.com',
-            target: '_blank',
-          },
-        ],
-        isExpanded: false,
-      },
-      {
-        name: 'Parent link 2',
-        url: 'http://example.com',
-        target: '_blank',
-        expandAriaLabel: 'Show more Parent link 2',
-        links: [
-          {
-            name: 'Child link 4',
-            url: 'http://example.com',
-            target: '_blank',
-          },
-        ],
-      },
-    ],
-  },
-];
+
+
 export interface IHelloWorldProps {
   name?: string;
 }
 
 export class HelloWorld extends React.Component<IHelloWorldProps> {
+  context: ComponentFramework.Context<IInputs>
+  navStyles: Partial<INavStyles> = {
+    root: {
+      width: 208,
+      height: 350,
+      boxSizing: 'border-box',
+      border: '1px solid #eee',
+      overflowY: 'auto',
+    },
+  };
+  navLinkGroups: INavLinkGroup[] = [
+  
+    {
+      links: [
+        {
+          name: 'Child link 1',
+          url: 'http://example.com',
+          target: '_blank',
+        },
+        {
+          name: 'Parent link 1',
+          url: '',
+          target: '_blank',
+          expandAriaLabel: 'Show more Parent link 1',
+          links: [
+            {
+              name: 'Child link 1',
+              url: 'http://example.com',
+              target: '_blank',
+            },
+            {
+              name: 'Child link 2',
+              url: 'http://example.com',
+              target: '_blank',
+              expandAriaLabel: 'Show more Child link 2',
+              links: [
+                {
+                  name: '3rd level link 1',
+                  url: 'http://example.com',
+                  target: '_blank',
+                },
+                {
+                  name: '3rd level link 2',
+                  url: 'http://example.com',
+                  target: '_blank',
+                },
+              ],
+            },
+            {
+              name: 'Child link 3',
+              url: 'http://example.com',
+              target: '_blank',
+            },
+          ],
+          isExpanded: false,
+        },
+        {
+          name: 'Parent link 2',
+          url: 'http://example.com',
+          target: '_blank',
+          expandAriaLabel: 'Show more Parent link 2',
+          links: [
+            {
+              name: 'Child link 4',
+              url: 'http://example.com',
+              target: '_blank',
+            },
+          ],
+        },
+      ],
+    },
+  ];
   public render(): React.ReactNode {
 
-   return <Nav ariaLabel="Nav example with nested links" styles={navStyles} groups={navLinkGroups} />;
+   return <Nav ariaLabel="Nav example with nested links" styles={this.navStyles} groups={this.navLinkGroups} />;
   }
 }
